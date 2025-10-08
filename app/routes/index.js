d40f045d1be60db207d67c5f357eda624c21d34c
Francisco Meza <franksd2013@gmail.com>
2025-10-07 22:54:23 -0300

    Commit navigator
    Se integro navigator al sistema,

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,18 @@
 ﻿import { Router } from 'express';
 
 import homeController from '../controllers/homeController.js';
+// Importar nuevos controladores
+//import productController from '../controllers/productController.js';
+//import qrController from '../controllers/qrController.js';
+//import userController from '../controllers/userController.js';
 
 const router = Router();
 
 router.get('/', homeController.index);
 
-export default router;
+// Nuevas rutas para la navegación
+//router.get('/productos', productController.index);
+//router.get('/qr', qrController.index);
+//router.get('/usuario', userController.index);
 
+export default router;
