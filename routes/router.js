f4799e4899018de68247a6da4b1a994d59cf11fb
SeitaHuanchi <116596411+SeitaHuanchi@users.noreply.github.com>
2025-10-27 20:25:50 -0300

    arreglos simples
    Arregle que la barra de abajo donde esta el qr y demas no apareza en el login y en el registro, y aparte agrege accesos directos en el login y registro para que se pase directamente a ellos aparte deje el dashboard que cuando no estas iniciado sesion te salga el iniciar sesion para que puedas ver lo que quieras del dashboard

--- routes/router.js
+++ routes/router.js
@@ -12,6 +12,7 @@ const routes = {
         '/home_client': serveView('home_client.html'),
         '/home_admin': serveView('home_admin.html'),
         '/login': serveView('login.html'),
+        '/usuario': serveView('login.html'),
         '/register': serveView('register.html'),
         '/dashboard': serveView('dashboard.html'),
         '/qr': serveView('qr.html'),
